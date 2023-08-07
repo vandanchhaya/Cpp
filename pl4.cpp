938cf81d8d8f21d90e62cfd273eba2e8e3478360
vandanchhaya <141611062+vandanchhaya@users.noreply.github.com>
2023-08-07 12:02:17 +0530

    Add files via upload

---
+++ pl4.cpp
@@ -0,0 +1,20 @@
+//Write a C program to enter radius of a circle and find its diameter,circumference and area
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r,dia,circum,area;
+
+    cout<<"Enter the radius of circle : ";
+    cin>>r;
+
+    dia=2*r;
+    circum=2*3.14*r;
+    area=3.14*r*r;
+
+    cout<<"Diameter of circle is :: "<<dia<<endl;
+    cout<<"Circumference of circle is :: "<<circum<<endl;
+    cout<<"Area of circle is :: "<<area<<endl;
+
+    return 0;
+}
