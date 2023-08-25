197ad808dc4492ee7bfb1529ce4da06aa0c5ae4e
vandanchhaya <141611062+vandanchhaya@users.noreply.github.com>
2023-08-26 00:27:22 +0530

    Add files via upload

---
+++ p20.cpp
@@ -0,0 +1,30 @@
+//Write C++ program to find factorial of the given number with the use of function
+#include <iostream>
+using namespace std;
+int factorial(int n) 
+{
+    if (n == 0 || n == 1) 
+    {
+        return 1;
+    } 
+    else 
+    {
+        return n * factorial(n - 1);
+    }
+}
+int main() 
+{
+    int num;
+    cout << "Enter any positive number : ";
+    cin >> num;
+    if (num < 0) 
+    {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } 
+    else 
+    {
+        int fact = factorial(num);
+        cout << "Factorial of " << num << " is " << fact << endl;
+    }
+    return 0;
+}
